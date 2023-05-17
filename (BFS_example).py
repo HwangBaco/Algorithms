941405c31a142f4c804ba9bd440c91e2615b0459
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-05-17 17:14:36 +0900

    solved

--- (BFS_example).py
+++ (BFS_example).py
@@ -1,3 +1,15 @@
+"""
+파이썬의 BFS는 그래프에 대한 넓/이/우/선/탐/색 입니다.
+
+구현 과정은 기본적으로 큐를 이용하게 되며, 현실적으론 deque를 사용하면 된다.
+
+그래프를 받을 때, 각 노드의 번호를 인덱스와 일치시키고, 각 노드의 엣지를
+리스트에 등록해둬야 한다. 만약 (3,7)이면 li[3].append(7) and li[7].append(3)
+
+그리고 일반적인 경우, deque에 append하는 순간은 해당 그래프의 노드를 들르는 순간과 같으므로
+visited[i]에도 True를 먹여줘야 한다.
+"""
+
 from collections import deque
 
 def bfs(graph, start, visited):
@@ -5,7 +17,7 @@ def bfs(graph, start, visited):
     visited[start] = True
     while queue:
         v = queue.popleft()
-        print(v, end=' ')
+        # print(v, end=' ')
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
