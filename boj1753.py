c5ea47a05b1386d10a240b03e8d38891550a278d
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-05-19 15:46:14 +0900

    bfs

---
+++ boj1753.py
@@ -0,0 +1,35 @@
+"""
+수빈 N : 0 <= N <= 100,000
+동생 K : 0 <= K <= 100,000
+
+수빈이는 걷거나 순간이동 가능
+수빈이의 위치가 x일 때 걷는다면 단위 시간당 x-1, x+1로 이동 / 순간이동시 2x
+수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간은?
+
+DP
+여러 경우의 수 중에서 가장 짧은 수를 찾으면 됨
+작은 수에서 조절하고 두배 튕기고 하나 증감하는게 효율적임
+
+이번에,
+-1 : 한칸 전에 target이 있는 경우 or 두 배를 했을 때
++1 : 한칸 이후 target이 있는 경우 or 
+x2
+"""
+from collections import deque as dq
+def bfs():
+    queue.append(n)
+    while queue:
+        v = queue.popleft()
+        if v == k:
+            print(dist[v])
+            return
+        for next in (v-1, v+1, v*2):
+            if 0 <= next <= MAX and not dist[next]:
+                dist[next] = dist[v] + 1
+                queue.append(next)
+MAX = 10**5
+n, k = map(int, input().split())
+dist = [0] * (MAX+1)
+queue = dq()
+
+bfs()
