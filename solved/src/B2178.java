e51ad7c8cc393f9ce014a3c4bc5744780ae732c1
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-05-31 11:11:42 +0900

    BFS

--- solved/src/B2178.java
+++ solved/src/B2178.java
@@ -10,10 +10,11 @@ import java.util.StringTokenizer;
 public class B2178 {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static boolean [][] visited;
     static int[][] arr;
+    static boolean[][] visited;
     static int n, m;
     public static void main(String[] args) throws IOException {
+        // 기본 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -24,38 +25,35 @@ public class B2178 {
         visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+            String s = st.nextToken();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(line.substring(j, j + 1));
+                arr[i][j] = Integer.parseInt(s.substring(j, j + 1));
             }
         }
-
         BFS(0, 0);
         bw.write(Integer.toString(arr[n - 1][m - 1]));
         bw.flush();
-        br.close();
         bw.close();
+        br.close();
     }
 
     private static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{i, j});
-        while (!queue.isEmpty()) {
-            int now[] = queue.poll();
-            visited[i][j] = true;
-            for (int k = 0; k < 4; k++) { // 상하좌우 탐색
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {i, j});
+        visited[i][j] = true;
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            for (int k = 0; k < 4; k++) {
                 int x = now[0] + dx[k];
                 int y = now[1] + dy[k];
-                if (x >= 0 && y >= 0 && x < n && y < m) { // 배열을 넘어가면 안되고
-                    if (arr[x][y] != 0 && !visited[x][y]) { // 0이여서 갈 수 없거나, 방문한 곳이면 안됨
-                        // 이제 갈 수 있는 곳이다.
-                        visited[x][y] = true;
-                        arr[x][y] = arr[now[0]][now[1]] + 1; // 핵심
-                        queue.add(new int[]{x, y});
-                    }
+                if (x >= 0 && y >= 0 && x < n && y < m && !visited[x][y] && arr[x][y] != 0) {
+                    arr[x][y] = arr[now[0]][now[1]] + 1;
+                    q.offer(new int[]{x, y});
+                    visited[x][y] = true;
                 }
             }
         }
+
     }
 
 }
