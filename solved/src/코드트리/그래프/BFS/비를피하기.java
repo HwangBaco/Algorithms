59b0cfb5d6e44c97048ff233fadf408edc5a0c78
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-10-11 23:41:23 +0900

    BFS

---
+++ solved/src/코드트리/그래프/BFS/비를피하기.java
@@ -0,0 +1,16 @@
+package 코드트리.그래프.BFS;
+
+import java.io.IOException;
+
+public class 비를피하기 {
+    public static void main(String[] args) throws IOException {
+        /*
+        * 숫자 0, 1,2,3으로만 이루어진 nxn 격자
+        * 0 : 이동가능 , 1 : 이동불가 , 2 : 사람있음 , 3 : 비 피하는 공간
+        * h명의 사람이 겹치지 않게 서 있고
+        * m개의 공간 위치가 주어졌을 때
+        * 사람마다 비를 피할 수 있는 가장 가까운 공간까지의 거리는?
+        * 
+        * */
+    }
+}
