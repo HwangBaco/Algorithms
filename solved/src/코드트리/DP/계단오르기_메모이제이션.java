b679a5c517ce6a2b71ac0093b23f6243645fb5f3
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-10-18 10:54:03 +0900

    DP

---
+++ solved/src/코드트리/DP/계단오르기_메모이제이션.java
@@ -0,0 +1,41 @@
+package 코드트리.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 계단오르기_메모이제이션 {
+    private static int n;
+    private static int[] dp;
+    public static final int MAX_N = 1001;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        dp = new int[MAX_N];
+        for (int i = 0; i < MAX_N; i++) {
+            dp[i] = -1;
+        }
+        int ans = memoization(n);
+        if (ans == -1) {
+            System.out.println(0);
+        } else {
+            System.out.println(ans);
+        }
+    }
+
+    private static int memoization(int i) {
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        if (i == 1) {
+            return 0;
+        } else if (i == 2 || i == 3) {
+            return 1;
+        }
+        dp[i] = memoization(i - 2) + memoization(i - 3);
+        return dp[i];
+
+    }
+}
