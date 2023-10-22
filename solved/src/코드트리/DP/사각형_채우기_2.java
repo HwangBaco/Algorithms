f24da7c038344d34193443bd2d6a33f15b588a33
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-10-22 12:51:42 +0900

    DP

--- solved/src/코드트리/DP/사각형_채우기_2.java
+++ solved/src/코드트리/DP/사각형_채우기_2.java
@@ -4,26 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 사각형_채우기_3 {
+public class 사각형_채우기_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[1001];
+        int[] dp = new int[1001];
         dp[0] = 1;
-        dp[1] = 2;
-        dp[2] = 7;
-
+        dp[1] = 1;
+        dp[2] = 3;
         for (int i = 3; i <= 1000; i++) {
-            dp[i] = mod(mod(2 * dp[i - 1]) + mod(3 * dp[i - 2]));
-            for (int j = i-3; j >= 0; j--) {
-                dp[i] = mod(dp[i] + mod(2 * dp[j]));
-            }
+            dp[i] = (dp[i - 1] + (2 * dp[i - 2]) % 10007) % 10007;
         }
         System.out.println(dp[n]);
 
-    }
-
-    private static long mod(long num) {
-        return num % 1000000007;
     }
 }
