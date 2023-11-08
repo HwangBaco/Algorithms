2868fc154a090df63c3f4078504b39110360fa65
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-09 06:37:08 +0900

    dp

--- solved/src/코드트리/DP/정수_사각형_최소_합.java
+++ solved/src/코드트리/DP/정수_사각형_최소_합.java
@@ -33,17 +33,13 @@ public class 정수_사각형_최소_합 {
             dp[i][n-1] = arr[i][n-1] + dp[i-1][n-1];
         }
 
+        // tabulation
         for (int i = 1; i < n; i++) {
             for (int j = n-2; j >= 0; j--) {
                 dp[i][j] = Math.min(dp[i - 1][j] + arr[i][j], dp[i][j + 1] + arr[i][j]);
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
+
         System.out.println(dp[n-1][0]);
     }
 }
