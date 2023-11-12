02512524fea722b3c63fc4412240605902395214
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-12 15:15:11 +0900

    dp

---
+++ solved/src/코드트리/DP/최대_점프_횟수.java
@@ -0,0 +1,41 @@
+package 코드트리.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대_점프_횟수 {
+    private static int n;
+    private static int[] arr;
+    private static int[] dp;
+    private static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((j + arr[j]) >= i) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            if (dp[i] == 0) {
+                break;
+            }
+            ans = Math.max(dp[i], ans);
+        }
+        System.out.println(ans);
+    }
+}
