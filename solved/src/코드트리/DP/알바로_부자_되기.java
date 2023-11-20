76894fc8a400dedbadea13fbf75bcb7e00afdde1
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-20 10:56:27 +0900

    dp

---
+++ solved/src/코드트리/DP/알바로_부자_되기.java
@@ -0,0 +1,5 @@
+package 코드트리.DP;
+
+public class 알바로_부자_되기 {
+
+}
--- solved/src/코드트리/DP/이차원_최대_증가_수열.java
+++ solved/src/코드트리/DP/이차원_최대_증가_수열.java
@@ -1,5 +1,3 @@
-package 코드트리.DP;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,7 +5,7 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class 이차원_최대_증가_수열 {
+public class Main {
     private static int n, m;
     private static int[][] arr;
     private static int[][] dp;
@@ -29,32 +27,18 @@ public class 이차원_최대_증가_수열 {
 
         // init
         dp[0][0] = 1;
-        int ans = 0;
         pq.add(-1);
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = 0;
-        }
-        for (int i = 1; i < m; i++) {
-            dp[0][i] = 0;
-        }
 
-        for (int i = 1; i < n; i++) {
-            if (arr[i][1] > arr[0][0]) {
-                dp[i][1] = 2;
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            if (arr[1][i] > arr[0][0]) {
-                dp[1][i] = 2;
-            }
-        }
 
         // tabulation
-        for (int i = 2; i < n; i++) {
-            for (int j = 2; j < m; j++) {
-                for (int k = 1; k < i; k++) {
-                    for (int l = 1; l < j; l++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < i; k++) {
+                    for (int l = 0; l < j; l++) {
                         if (arr[i][j] > arr[k][l]) {
+                            if(dp[k][l] == 0)
+                                continue;
+
                             dp[i][j] = Math.max(dp[i][j], dp[k][l] + 1);
                             pq.add(-dp[i][j]);
                         }
