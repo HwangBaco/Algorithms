b236629369a3b785401abfa47d73ab7ed1a2309e
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-23 22:56:49 +0900

    dp

--- solved/src/코드트리/DP/알바로_부자_되기.java
+++ solved/src/코드트리/DP/알바로_부자_되기.java
@@ -1,5 +1,16 @@
 package 코드트리.DP;
 
-public class 알바로_부자_되기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+public class 알바로_부자_되기 {
+    private static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+    }
 }
