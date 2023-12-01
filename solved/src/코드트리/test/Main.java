cc68eec969b471d516c5cfe0ea50aa8a5a76d7af
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-12-01 16:40:42 +0900

    linkedlist

--- solved/src/코드트리/test/Main.java
+++ solved/src/코드트리/test/Main.java
@@ -1,82 +1,43 @@
 package 코드트리.test;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     private static int n;
-    private static int[][] arr;
-    private static boolean[][] visited;
-
-    private static boolean hasSize;
-    private static Map<Integer, Integer> m = new HashMap<>();
-
+    private static List<Integer> li = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        arr = new int[n][n];
-        visited = new boolean[n][n];
+        go(false);
+    }
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+    private static void go(boolean hasPrevZero) {
+        if (li.size() == n) {
+            li.forEach(System.out::print);
+            System.out.println();
+            return;
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    hasSize = false;
-                    visited[i][j] = true;
-                    dfs(new Pair(i, j), arr[i][j]);
-                    if (hasSize) {
-                        m.putIfAbsent(arr[i][j], 1);
-                        m.replace(arr[i][j], m.get(arr[i][j]) + 1);
-                    }
+        for (int i = 1; i >= 0; i--) {
+            if (hasPrevZero && i == 0) {
+                li.add(1);
+                go(false);
+                li.remove(li.size() - 1);
+            } else {
+                if (i == 1) {
+                    hasPrevZero = false;
+                } else {
+                    hasPrevZero = true;
                 }
-            }
-        }
-        for (int i = 1; i <= 3; i++) {
-            System.out.print(m.get(i) + " ");
-        }
-
-
-
-    }
-
-    private static void dfs(Pair p, int type) {
-        int[] dy = new int[]{1, 0, -1, 0};
-        int[] dx = new int[]{0, 1, 0, -1};
-
-        for (int i = 0; i < 4; i++) {
-            int newY = p.y + dy[i];
-            int newX = p.x + dx[i];
-
-            if (canGo(newY, newX, type)) {
-                visited[newY][newX] = true;
-                dfs(new Pair(newY, newX), type);
-                hasSize = true;
+                li.add(i);
+                go(hasPrevZero);
+                li.remove(li.size() - 1);
             }
         }
     }
-
-    private static boolean inRange(int y, int x) {
-        return x >= 0 && x < n && y >= 0 && y < n;
-    }
-
-    private static boolean canGo(int y, int x, int type) {
-        return inRange(y, x) && arr[y][x] == type && !visited[y][x];
-    }
-
-    private static class Pair {
-        private int x, y;
-
-        private Pair(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-
 }
