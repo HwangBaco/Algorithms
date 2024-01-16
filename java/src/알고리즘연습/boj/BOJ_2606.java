1f9095a7dacbdb66600d4959dddb99e50bbbc2d6
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2024-01-16 20:22:20 +0900

    설정 추가

--- java/src/알고리즘연습/boj/BOJ_2606.java
+++ java/src/알고리즘연습/boj/BOJ_2606.java
@@ -6,9 +6,14 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_2606 {
+    /*
+    * 시간 : DFS 124ms, BFS 132ms
+    * 메모리 : DFS 14292kb, BFS 14272kb
+    * 로직 : graph size 구하는 문제여서, 일단 dfs로 먼저 풀고, bfs로도 해 봤습니다.
+    * */
+    
     static List<Integer>[] adj;
     static boolean[] visited;
-
     static int graphSize;
 
     public static void main(String[] args) throws IOException {
