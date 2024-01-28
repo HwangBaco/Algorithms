5ab07669b4f44913371513eb229d7c8a9ac16380
황제철 <ghkdwp018@gmail.com>
2024-01-28 16:51:48 +0900

    impl

--- boj/BOJ_14719_빗물_COPY.java
+++ boj/BOJ_14719_빗물_COPY.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 public class BOJ_14719_빗물_COPY {
+    /*
+    * time complexity : O(N^2)
+    * */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +18,9 @@ public class BOJ_14719_빗물_COPY {
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[w];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < w; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] arr = Stream.of(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
         int res = 0;
 
