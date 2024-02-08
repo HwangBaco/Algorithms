f4c2703b5daa2b9a158af03036d90a36aaaac619
황제철 <ghkdwp018@gmail.com>
2024-02-08 09:23:41 +0900

    bruteforce

--- boj/BOJ_27921_동전퍼즐.java
+++ boj/BOJ_27921_동전퍼즐.java
@@ -5,50 +5,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_27921 {
+public class BOJ_27921_동전퍼즐 {
+    /*
+    * 시간복잡도 : O(N^4) ; N이 최대 10이여서 가능했습니다...
+    *
+    * 실행 시간 : 124 ms
+    * 사용 메모리 : 14276 KB
+    * */
     public static void main(String[] args) throws IOException {
+        int[] arr = new int[30];
+        int[] brr;
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int aCnt = 0;
         int H1 = Integer.parseInt(st.nextToken());
         int W1 = Integer.parseInt(st.nextToken());
-        int[] arr = new int[H1];
-        int aCount = 0;
         for (int i = 0; i < H1; i++) {
             String s = br.readLine();
             for (int j = 0; j < W1; j++) {
-                if (s.substring(j, j + 1).equals("O")) {
-                    arr[i] |= 1 << j;
-                    aCount++;
+                String ss = s.substring(j, j + 1);
+                if (ss.equals("O")) {
+                    arr[10 + i] |= 1 << (j + 10);
+                    aCnt++;
                 }
+
             }
         }
-
         st = new StringTokenizer(br.readLine());
         int H2 = Integer.parseInt(st.nextToken());
         int W2 = Integer.parseInt(st.nextToken());
-        int[] brr = new int[H2];
+        brr = new int[H2];
         for (int i = 0; i < H2; i++) {
             String s = br.readLine();
             for (int j = 0; j < W2; j++) {
-                if (s.substring(j, j + 1).equals("O")) {
+                String ss = s.substring(j, j + 1);
+                if (ss.equals("O")) {
                     brr[i] |= 1 << j;
                 }
             }
         }
         int ans = (int) 1e9;
-        for (int i = 0; i < H1; i++) {
-            for (int j = 0; j < W1; j++) {
-                int res = aCount;
-                for (int k = i; k < i + H2; k++) {
-                    for (int l = j; l < j + W2; l++) {
-                        if (k < H1 && l < W1) { // 기본 조건
-                            if ((arr[k] & 1 << l) != 0 && (brr[k - i] & 1 << (l - j)) != 0) {
-                                res--;
-                            }
+        for (int i = 0; i < 20; i++) {      // startY 좌표
+            for (int j = 0; j < 20; j++) {  // startX 좌표
+                int res = aCnt;
+                for (int k = 0; k < H2; k++) {
+                    for (int l = 0; l < W2; l++) {
+                        if ((arr[i + k] & 1 << (j + l)) != 0 && (brr[k] & 1 << l) != 0) {
+                            res--;
                         }
                     }
                 }
-
                 ans = Math.min(ans, res);
             }
         }
