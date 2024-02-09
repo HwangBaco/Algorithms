d748cd98f9ab46ce0529eaff9c4ffdddd9e8d2e7
황제철 <ghkdwp018@gmail.com>
2024-02-10 00:55:11 +0900

    simulation

--- boj/BOJ_11559_뿌요뿌요.java
+++ boj/BOJ_11559_뿌요뿌요.java
@@ -3,17 +3,23 @@ package 알고리즘연습.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ_11559_뿌요뿌요 {
+    /*
+     * 실행시간 : 128 ms
+     *
+     * 메모리 : 14108 KB
+     *
+     * 로직 : DFS 이용해서 size 4 이상이면 터뜨리고 int[] temp 사용해서 중력 적용
+     * */
     public static final int HEIGHT = 12;
     public static final int WIDTH = 6;
     private static int[] visited = new int[HEIGHT];
     private static String[][] grid = new String[HEIGHT][WIDTH];
     private static int[] dy = {-1, 1, 0, 0};
     private static int[] dx = {0, 0, -1, 1};
-    private static boolean isBomb;
     private static boolean hasChain;
+    private static boolean hasBomb;
     private static int depth;
 
     public static void main(String[] args) throws IOException {
@@ -24,65 +30,74 @@ public class BOJ_11559_뿌요뿌요 {
                 grid[i][j] = s.substring(j, j + 1);
             }
         }
+        br.close();
+
         int chainCnt = 0;
+
         while (true) {
-
-            visited = new int[HEIGHT];
-            // 터뜨리기
-            for (int i = 0; i < HEIGHT; i++) {
-                for (int j = 0; j < WIDTH; j++) {
-                    if (!grid[i][j].equals(".") && (visited[i] & 1 << j) == 0) {
-                        // dfs 돌입 전 터지는 여부 초기화
-                        isBomb = false;
-                        depth = 0;
-                        dfs(i, j, grid[i][j]);
-                    }
-                }
-            }
-            for (int i = 0; i < WIDTH; i++) {
-                String[] temp = new String[HEIGHT]; // 중력 업데이트용
-                // 현황 체크
-                int tempIdx = 0;
-                for (int j = HEIGHT - 1; j >= 0; j--) {
-                    if (!grid[j][i].equals(".")) {
-                        temp[tempIdx++] = grid[j][i];
-                    }
-                }
-                // 중력 반영
-                int idx = 0;
-                for (int j = HEIGHT - 1; j >= 0; j--) {
-                    grid[j][i] = temp[idx] == null ? "." : temp[idx];
-                    idx++;
-                }
-            }
-            for (int i = 0; i < HEIGHT; i++) {
-                for (int j = 0; j < WIDTH; j++) {
-                    System.out.print(grid[i][j]);
-
-                }
-                System.out.println();
-
-            }
-            System.out.println("=================");
-
+            findBomb();
+            gravity();
             if (!hasChain) {
-                System.out.println("END");
                 break;
             }
-
-
             hasChain = false;
             chainCnt++;
-
         }
-        System.out.println(chainCnt);
 
+        System.out.println(chainCnt);
+    }
+
+    private static void findBomb() {
+        // 터뜨리기
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                if (!grid[i][j].equals(".")) {
+                    // dfs 돌입 전 터지는 여부 초기화
+                    depth = 0;
+                    visited = new int[HEIGHT];
+                    hasBomb = false;
+                    dfs(i, j, grid[i][j]);
+                }
+                if (hasBomb) {
+                    executeBomb();
+                }
+            }
+        }
+    }
+
+    private static void executeBomb() {
+        for (int k = 0; k < HEIGHT; k++) {
+            for (int l = 0; l < WIDTH; l++) {
+                if ((visited[k] & 1 << l) != 0) {
+                    grid[k][l] = ".";
+                }
+            }
+        }
+    }
+
+    private static void gravity() {
+        for (int i = 0; i < WIDTH; i++) {
+            String[] temp = new String[HEIGHT]; // 중력 업데이트용
+            // 현황 체크
+            int tempIdx = 0;
+            for (int j = HEIGHT - 1; j >= 0; j--) {
+                if (!grid[j][i].equals(".")) {
+                    temp[tempIdx++] = grid[j][i];
+                }
+            }
+            // 중력 반영
+            int idx = 0;
+            for (int j = HEIGHT - 1; j >= 0; j--) {
+                grid[j][i] = temp[idx] == null ? "." : temp[idx];
+                idx++;
+            }
+        }
     }
 
     private static void dfs(int y, int x, String color) {
         if (depth >= 4) {
-            isBomb = true;
             hasChain = true;
+            hasBomb = true;
         }
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
@@ -93,10 +108,7 @@ public class BOJ_11559_뿌요뿌요 {
                 dfs(ny, nx, color);
             }
         }
-        // 상하좌우로 연결된 개수가 4 이상인 경우 전부 터뜨림.
-        if (isBomb) {
-            grid[y][x] = ".";
-        }
+
     }
 
     private static boolean canGo(int y, int x, String color) {
