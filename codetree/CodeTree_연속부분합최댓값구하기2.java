95a3db88dfef2851d347c10d33329c592dd837ae
황제철 <ghkdwp018@gmail.com>
2024-02-10 20:46:12 +0900

    greedy

--- codetree/CodeTree_연속부분합최댓값구하기2.java
+++ codetree/CodeTree_연속부분합최댓값구하기2.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 import java.util.stream.Stream;
 
 public class CodeTree_연속부분합최댓값구하기2 {
+    /*
+    * 알고리즘 : 그리디
+    * 
+    * 구간 내 최대값을 구할 때, 누적합이 음수로 되는 부분은 절대 최대가 될 수 없음을 이용하여 그리디로 풀이
+    * */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
