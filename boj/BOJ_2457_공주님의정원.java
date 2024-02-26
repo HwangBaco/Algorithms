3f27bd540b196e97cc09459061880fc0b62190da
황제철 <ghkdwp018@gmail.com>
2024-02-26 18:50:18 +0900

    greedy

--- boj/BOJ_2457_공주님의정원.java
+++ boj/BOJ_2457_공주님의정원.java
@@ -30,6 +30,7 @@ public class BOJ_2457_공주님의정원 {
             }
             return o1.startMonth - o2.startMonth;
         }));
+
         PriorityQueue<Date> pq = new PriorityQueue<>((o1, o2) -> { // 끝나는 날짜 내림차순
             if (o1.endMonth == o2.endMonth) {
                 return o2.endDay - o1.endDay;
@@ -42,8 +43,7 @@ public class BOJ_2457_공주님의정원 {
         int cnt = 0;
         // 가능한 날짜들을 전부 pq에 담고
         for (Date d : arr) {
-            if (stdMonth > 11 || stdMonth == 11 && stdDay >= 30) {
-//                System.out.println(stdMonth + " " + stdDay);
+            if (stdMonth > 11 || stdMonth == 11 && stdDay >= 31) {
                 System.out.println(cnt);
                 return;
             }
@@ -51,20 +51,15 @@ public class BOJ_2457_공주님의정원 {
             int sd = d.startDay;
             int em = d.endMonth;
             int ed = d.endDay;
-            if (!(stdMonth > sm || (stdMonth == sm && (stdDay + 1) >= sd))) { // 기준일을 넘어서 선택해야 하는 경우
+            if (!(stdMonth > sm || (stdMonth == sm && (stdDay) >= sd))) { // 기준일을 넘어서 선택해야 하는 경우
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                    return;
+                }
                 Date poll = pq.poll();
                 stdMonth = poll.endMonth;
-                stdDay = poll.endDay - 1;
-                if (stdDay == 0) {
-                    stdMonth--;
-                    if (stdMonth == 1 || stdMonth == 3 || stdMonth == 5 || stdMonth == 7 || stdMonth == 8 || stdMonth == 10 || stdMonth == 12) {
-                        stdDay = 31;
-                    } else if (stdMonth == 2) {
-                        stdDay = 28;
-                    } else {
-                        stdDay = 30;
-                    }
-                }
+                stdDay = poll.endDay;
+
                 cnt++;
 //                System.out.println(stdMonth + " " + stdDay);
 
@@ -75,7 +70,8 @@ public class BOJ_2457_공주님의정원 {
                     return o2.endMonth - o1.endMonth;
                 });
             }
-            if ((stdMonth > sm || (stdMonth == sm && (stdDay + 1) >= sd))) {
+
+            if ((stdMonth > sm || (stdMonth == sm && (stdDay) >= sd))) {
                 // 아직 기준일에 미치지 않는 경우 == 가능한 경우
                 pq.add(d); // 우선순위 큐에 담아둠
 
@@ -85,33 +81,22 @@ public class BOJ_2457_공주님의정원 {
             }
 
         }
-        if (stdMonth > 11 || stdMonth == 11 && stdDay >= 30) {
+        if (stdMonth > 11 || stdMonth == 11 && stdDay >= 31) {
             System.out.println(cnt);
             return;
         }
-        Date poll = pq.poll();
-        stdMonth = poll.endMonth;
-        stdDay = poll.endDay - 1;
-        if (stdDay == 0) {
-            stdMonth--;
-            if (stdMonth == 1 || stdMonth == 3 || stdMonth == 5 || stdMonth == 7 || stdMonth == 8 || stdMonth == 10 || stdMonth == 12) {
-                stdDay = 31;
-            } else if (stdMonth == 2) {
-                stdDay = 28;
-            } else {
-                stdDay = 30;
-            }
+        if (!pq.isEmpty()) {
+            Date poll = pq.poll();
+            stdMonth = poll.endMonth;
+            stdDay = poll.endDay;
         }
-//        System.out.println(stdMonth + " " + stdDay);
+
         cnt++;
-        if (!(stdMonth > 11 || stdMonth == 11 && stdDay >= 30)) {
+        if (!(stdMonth > 11 || stdMonth == 11 && stdDay >= 31)) {
             System.out.println(0);
             return;
         }
         System.out.println(cnt);
-
-
-        // 불가능해진 시점에 pq.poll()해서 시작일 갱신 + cnt++;
     }
 
     private static class Date {
