44dd2b3ec7febfa4d3d156c8f6e078bb737e8eff
황제철 <ghkdwp018@gmail.com>
2024-03-23 23:17:00 +0900

    sort

---
+++ boj/BOJ_11650_좌표정렬하기.java
@@ -0,0 +1,38 @@
+package 알고리즘연습.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_11650_좌표정렬하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i] = new int[]{x, y};
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] ints : arr) {
+            sb.append(ints[0]).append(" ").append(ints[1]).append("\n");
+        }
+        System.out.println(sb.toString());
+
+    }
+}
