1be7cd8bc60d43beac7b8be8084bb65fe7b83ad8
황제철 <ghkdwp018@gmail.com>
2024-03-30 17:17:33 +0900

    two pointer

--- leetcode/LeetCode_2962.java
+++ leetcode/LeetCode_2962.java
@@ -1,8 +1,12 @@
 package 알고리즘연습.leetcode;
 
 import java.util.Arrays;
-
 public class LeetCode_2962 { // Count Subarrays Where Max Element Appears at Least K Times
+    /**
+     * @param nums given number arrray
+     * @param k the number of times at least the max element appears on each subarray
+     * @return the number of subarrays where the maximum element of nums appears at least k times in that subarray
+     */
     public long countSubarrays(int[] nums, int k) {
         int maxVal = Arrays.stream(nums).max().getAsInt();
 
