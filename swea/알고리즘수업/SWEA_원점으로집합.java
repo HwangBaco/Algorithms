0e7e7d0ac7dc62ff28e7923da031ff77f340aed0
황제철 <ghkdwp018@gmail.com>
2024-04-02 19:06:39 +0900

    save

--- swea/알고리즘수업/SWEA_원점으로집합.java
+++ swea/알고리즘수업/SWEA_원점으로집합.java
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-package algorithm.오늘푼문제0401;
+package 알고리즘연습.swea.알고리즘수업;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -56,58 +55,3 @@ public class SWEA_원점으로집합 {
         sb.append(String.format("#%d %d\n", tc, i));
     }
 }
-=======
-package 알고리즘연습.swea.알고리즘수업;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class SWEA_원점으로집합 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            solution(br, sb, tc);
-        }
-        System.out.println(sb.toString());
-    }
-
-    private static void solution(BufferedReader br, StringBuilder sb, int tc) throws IOException {
-        StringTokenizer st;
-        long n = Integer.parseInt(br.readLine());
-        long max = -1;
-        int oddBit = 0;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            long a = Integer.parseInt(st.nextToken());
-            long b = Integer.parseInt(st.nextToken());
-            long abs = Math.abs(a) + Math.abs(b);
-            if (i == 0) {
-                oddBit = abs % 2 == 1 ? 1 : 0;
-            } else {
-                if (!(abs % 2 == oddBit)) {
-                    sb.append(String.format("#%d %d\n", tc, -1));
-                    return;
-                }
-            }
-            max = Math.max(max, abs);
-        }
-        // max가 구해진 상황
-        long i = 0;
-        long j = 0;
-        while (j < max) {
-            j += ++i;
-        }
-        j -= max;
-        while (j % 2 == 1) {
-            j += ++i;
-        }
-        sb.append(String.format("#%d %d\n", tc, i));
-    }
-}
->>>>>>> Stashed changes
