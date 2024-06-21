e2fbdbe061bcdf5314b865ef3765e0222ed0ca4a
Jerry <jerryhwang082@gmail.com>
2024-06-21 11:08:13 +0900

    [Gold IV] Title: 좋다, Time: 228 ms, Memory: 110568 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = sorted(list(map(int, input().split())))
+ans = 0
+for i in range(n):
+    left, right = 0, len(li)-2
+    temp = li[:i] + li[i+1:]
+    while left < right:
+        v = temp[left] + temp[right]
+        if v == li[i]:
+            ans += 1
+            break
+        elif v < li[i]:
+            left += 1
+        else :
+            right -= 1
+
+print(ans)
