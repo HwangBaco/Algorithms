6800b3ee3e685671807710c8d338dfd29176733b
Jerry <jerryhwang082@gmail.com>
2024-10-14 12:42:22 +0900

    Time: 7 ms (96.92%), Space: 11.7 MB (49.95%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        length = len(word1)
+        if length < len(word2):
+            length = len(word2)
+
+        ans = ""
+        for i in range(length):
+            if i < len(word1):
+                ans += word1[i]
+            if i < len(word2):
+                ans += word2[i]
+        return ans
