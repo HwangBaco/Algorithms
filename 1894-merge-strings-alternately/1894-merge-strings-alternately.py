0348c8fc0a4765891dfd0059d01e7e0caa15f6c9
Jerry <jerryhwang082@gmail.com>
2024-10-14 12:45:38 +0900

    Time: 10 ms (87.62%), Space: 11.7 MB (49.95%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -9,10 +9,10 @@ class Solution(object):
         if length < len(word2):
             length = len(word2)
 
-        ans = []
+        ans = ""
         for i in range(length):
             if i < len(word1):
-                ans.append(word1[i])
+                ans += word1[i]
             if i < len(word2):
-                ans.append(word2[i])
-        return ''.join(ans)
+                ans += word2[i]
+        return ans
